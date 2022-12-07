b04f4647ac17577c5f87b31fc056a4dd12bc439f
Mariana Perez <77807796+mariana-perezh@users.noreply.github.com>
2022-12-06 19:49:43 -0500

    Can render html

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
-import express from 'express';
-
-const db = require('better-sqlite3');
+import express, { Router } from 'express';
 const app = express();
+const router = express.Router();
 
-import minimist from minimist;
+import path from 'path';
+const __dirname = path.resolve();
+
+// import db from 'better-sqlite3';
+// const db = db();
+
+import minimist from 'minimist';
 const args = minimist(process.argv.slice(2));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-var port = 5000;
+var port = 5555;
+if (args.port) {
+    port = args.port;
+}
 
 
-app.listen(port);
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+} )
+
+
+app.use('/', router);
+
+
+app.listen(port, function(err) {
+    if(err) { 
+        console.log(err);
+        console.log("server listening on port: ", port);
+    }
+});
