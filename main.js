bf1be70065d243cc96aa359d3b0de28cc74703fc
Mariana Perez <77807796+mariana-perezh@users.noreply.github.com>
2022-12-06 22:28:16 -0500

    commented out

--- main.js
+++ main.js
@@ -21,28 +21,28 @@ addToDoButton.addEventListener('click', function(){
         }
     })
 
-    fetch('/clicked', {method: 'POST'})
-    .then(function(response) {
-        if(response.ok) {
-            console.log('Click was recorded');
-            return;
-        } throw new Error('Request failed.');
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
+    // fetch('/clicked', {method: 'POST'})
+    // .then(function(response) {
+    //     if(response.ok) {
+    //         console.log('Click was recorded');
+    //         return;
+    //     } throw new Error('Request failed.');
+    // })
+    // .catch(function(error) {
+    //     console.log(error);
+    // });
 });
 
-setInterval(function() {
-    fetch('/clicks', {method: 'GET'})
-      .then(function(response) {
-        if(response.ok) return response.json();
-        throw new Error('Request failed.');
-      })
-      .then(function(data) {
-        document.getElementById('counter').innerHTML = `Button was clicked ${data.length} times`;
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  }, 1000);
+// setInterval(function() {
+//     fetch('/clicks', {method: 'GET'})
+//       .then(function(response) {
+//         if(response.ok) return response.json();
+//         throw new Error('Request failed.');
+//       })
+//       .then(function(data) {
+//         document.getElementById('counter').innerHTML = `Button was clicked ${data.length} times`;
+//       })
+//       .catch(function(error) {
+//         console.log(error);
+//       });
+//   }, 1000);
