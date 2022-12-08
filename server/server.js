7971079d215fae5ef9b1db50b2545f3896460d73
Mariana Perez <77807796+mariana-perezh@users.noreply.github.com>
2022-12-07 21:19:27 -0500

    check server.js lines 29-33

--- server/server.js
+++ server/server.js
@@ -7,11 +7,33 @@ const __dirname = path.resolve();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-import {db} from './database.js'
 
-app.use(express.static('../client'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '..', 'views'));
+
+import sqlite3 from 'better-sqlite3'; 
+const db = new sqlite3('proj.db');
+db.pragma('journal_mode = WAL');
+
+const sqlInit = `CREATE TABLE users ( id INTEGER PRIMARY KEY AUTOINCREMENT, user VARCHAR, password VARCHAR );`
+try {
+    db.exec(sqlInit);
+    console.log("h")
+} catch (error) {}
+
+const sqlInit2 = `CREATE TABLE logs ( id INTEGER PRIMARY KEY AUTOINCREMENT, user VARCHAR, task VARCHAR, time VARCHAR);`
+try {
+    db.exec(sqlInit2);
+} catch (error) {}
+
+// TO GET OLD VIEW (NO DATABASE) UNCOMMMENT LINE BELOW
+// app.use(express.static('../client'));
+
+// TO GET NEW VIEW (WITH DATABASE) UNCOMMENT LINE BELOW
+app.use(express.static(path.join(__dirname, '..', 'views')));
 
 import minimist from 'minimist';
+import { fileURLToPath } from 'url';
 const args = minimist(process.argv.slice(2));
 
 app.use(express.json());
@@ -30,46 +52,98 @@ app.listen(port, function(err) {
 });
 
 app.get('/', (req, res) => {
-    res.send("Hello, world!");
+    res.render("index");
 });
 
-// create user endpoint
-app.post('/user/new/', (req, res, next) => {
-    let userData = {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    }
+app.post('/login', (req, res) => {
+    const user = req.body.username;
+    const password = req.body.password;
 
-    const stmt = db.prepare('INSERT INTO userinfo (username, email) VALUES (?, ?)');
-    const info = stmt.run(userData.username, userData.email);
-    res.status(200).json({"message": "user" + userData.username + "created"});
-    console.log(userData);
-    console.log(info);
-});
+    const timeElapsed = Date.now();
+    const today = newDate(timeElapsed);
 
-// read user endpoint
-app.get('/user/info/:username/', (req, res, next) => {
-    let userData = {
-        username: req.params.username
-    }
-})
+    const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'attempted to login' , '${today.toISOString()}');`;
 
-// delete user info enpoint
-app.delete('/user/bye', (req, res, next) => {
-    let userData = {
-        username: req.body.username
+    const stmt = db.prepare(`SELECT * FROM users WHERE user='${user}' and password= '${password}';`);
+    let row = stmt.get();
+    if (row === undefined) {
+        req.app.set('user', user);
+        req.app.set('password', password);
+    } else {
+        req.app.set('user', user);
+        req.app.set('password', password);
+        res.redirect('/index');
     }
 });
 
-// send some data endpoint
-app.post('/senddata/:username/', (req, res, next) => {
-    console.log(req.params.username + " logged in with this passwordL " + req.body.password)
-    res.status(200).json({
-        username: req.params.username,
-        password: req.body.password
-    }).end();
-})
+app.get('/index', function(req, res){
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
+
+    let user = req.app.get('user')
+
+    const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'succesful login or return to home', '${today.toISOString()}');`;
+    db.exec(stmt1)
+    res.render('index');
+});
+
+app.get('/acc_info', function(req, res){
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
+
+    let user = req.app.get('user')
+    
+    const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'viewed account info', '${today.toISOString()}');`;
+    db.exec(stmt1)
+    res.render('acc_info', {user: req.app.get('user'), password: req.app.get('password')});
+});
+
+app.post('/delete_acc', function(req, res){
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
+    let user1 = req.body.username
+    const stmt1 = `INSERT INTO logs (user, message, time) VALUES ('${user1}', 'deleted account', '${today.toISOString()}');`;
+    db.exec(stmt1)
+
+    const user = req.body.username;
+    const password = req.body.password;
+    const stmt = `DELETE FROM users WHERE user='${user}' and pass='${password}';`
+    db.exec(stmt)
+    res.render('acc_deleted');
+});
+
+app.get('/users_db', function(req, res){
+    const stmt = db.prepare(`SELECT * FROM users;`);
+    let all = stmt.all();
+
+    if(all === undefined) {
+        res.send('nothing in db');
+    } else {
+        res.send(all);
+    }
+});
+
+app.post('/newacc', function(req, res) {
+    const user = req.body.username;
+    const password = req.body.password;
+
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
+    const stmt2 = `INSERT INTO logs (user, message, time) VALUES ('${user}', 'tried to create new user', '${today.toISOString()}');`;
+    db.exec(stmt2)
+
+    const stmt1 = db.prepare(`SELECT * FROM users WHERE user='${user}'`);
+    let row = stmt1.get();
+
+    if (row === undefined) {
+        const stmt = `INSERT INTO users (user, password) VALUES ('${user}', '${password}');`;
+        db.exec(stmt)
+        res.render('new_acc_made');
+    } else {
+        res.render('username_exists')
+    }
+    
+});
 
 app.get('/clicks', (req, res) => {
     console.log("clicks");
@@ -78,17 +152,3 @@ app.get('/clicks', (req, res) => {
         click: "I clicked"
     });
 });
-
-app.post('/aclickss', async (req, res) => {
-    const body = req.body;
-    console.log(body);
-    res.send(body);
-});
-
-
-db.close((err) => { 
-    if (err) {
-        return console.error(err.message);
-    }
-    console.loge('Close the database connection');
-})
